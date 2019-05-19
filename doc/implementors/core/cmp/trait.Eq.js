5e64e293e6f6d2067374eb9509de679caaed7bf6
Deployment Bot (from Azure Pipelines) <deploy@tokio-rs.com>
2019-05-19 14:58:16 +0000

    Deploy Mio API documentation

---
+++ doc/implementors/core/cmp/trait.Eq.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mio"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"mio/struct.PollOpt.html\" title=\"struct mio::PollOpt\">PollOpt</a>",synthetic:false,types:["mio::event_imp::PollOpt"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"mio/struct.Ready.html\" title=\"struct mio::Ready\">Ready</a>",synthetic:false,types:["mio::event_imp::Ready"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"mio/struct.Interests.html\" title=\"struct mio::Interests\">Interests</a>",synthetic:false,types:["mio::event_imp::Interests"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"mio/event/struct.Event.html\" title=\"struct mio::event::Event\">Event</a>",synthetic:false,types:["mio::event_imp::Event"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"mio/unix/struct.UnixReady.html\" title=\"struct mio::unix::UnixReady\">UnixReady</a>",synthetic:false,types:["mio::sys::unix::ready::UnixReady"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html\" title=\"trait core::cmp::Eq\">Eq</a> for <a class=\"struct\" href=\"mio/struct.Token.html\" title=\"struct mio::Token\">Token</a>",synthetic:false,types:["mio::token::Token"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["mio"] = {"name":"","dirs":[{"name":"net","dirs":[],"files":["mod.rs","tcp.rs","udp.rs"]},{"name":"sys","dirs":[{"name":"unix","dirs":[],"files":["awakener.rs","dlsym.rs","epoll.rs","eventedfd.rs","io.rs","mod.rs","ready.rs","tcp.rs","udp.rs","uio.rs"]}],"files":["mod.rs"]}],"files":["event_imp.rs","io.rs","lazycell.rs","lib.rs","poll.rs","token.rs"]};
+createSourceSidebar();
